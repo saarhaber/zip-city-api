642170f3bdc2799282d60bc0989adfe0c7fd2e75
saarhaber <saarhaber@gmail.com>
2019-07-24 16:26:31 -0400

    zip-search

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Zip Search
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <City/>
     </div>
   );
 }
+
+class City extends React.Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    Zip: "",
+    data: [],
+  }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  async handleChange (event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  search = async () => {
+    this.setState({name: this.state.value})
+    const zip = this.state.Zip
+    try{ const { data } = await axios.get(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+    this.setState({data: data})}
+    catch(err) {
+      alert("no zip code exists")
+    }
+    };
+
+  render() {
+    const dataObj = this.state.data;
+    const list = dataObj.map((dataObj) =>
+    <ul>
+       {dataObj.City + "\n"}
+      <li> State: {dataObj.State + "\n"} </li>
+      <li> Location: ({dataObj.Lat + ","  + dataObj.Long +")\n"} </li>
+			<li> Population: {dataObj.EstimatedPopulation + "\n"} </li>
+      <li> Total Wages: {dataObj.TotalWages}</li>
+      </ul>
+			);
+    return (
+      <div>
+        <h3>Zip Code: <input type="text" name="Zip" 
+      onChange={this.handleChange}/></h3>
+        <button onClick={this.search}>Search</button>
+        <br></br><br></br>
+          {list}
+      </div>
+    );
+  }
+
+
+
+} // closing City
 
 export default App;
